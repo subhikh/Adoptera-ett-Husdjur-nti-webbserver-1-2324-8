001c301c3ccd2cc9afb5d29d7994b9c6fe93c181
subhikh <115615309+subhikh@users.noreply.github.com>
2024-04-02 08:45:07 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # Adoptera ett husdjur
+Project Name
+Adoptera ett Husdjur
+Description
+This project is a Flask application that allows users to browse and view information about different types of pets, including dogs, cats, and rabbits. The application uses a separate helper.py file to store the pet data.
+The application has the following routes:
+/: Displays the home page with links to the different types of pets.
+/animals/<string:pet_type>: Displays a list of all pets of the specified type.
+/animals/<string:pet_type>/<int:pet_id>: Displays the details of the specified pet.<H
+Installation
+To install this project, you will need to have Python 3.6 or later installed on your system. You can also install the project using pip, a package manager for Python. To install the project using pip, run the following command:
+Once the project is installed, you can run it using the following command:
+Usage
+To use this project, you will need to have Python 3.6 or later installed on your system. You can also install the project using pip, a package manager for Python. To install the project using pip, run the following command:
+Once the project is installed, you can run it using the following command:
+This will start the Flask development server on your local machine. You can then access the application at http://127.0.0.1:5000.
+The application has the following routes:
+/: Displays the home page with links to the different types of pets.
+/animals/<string:pet_type>: Displays a list of all pets of the specified type.
+/animals/<string:pet_type>/<int:pet_id>: Displays the details of the specified pet.
+You can use these routes to browse and view information about different types of pets.
+Options
+-h, --help: Show this help message and exit.
+-v, --version: Show the version of the application and exit.
+License
+This project is licensed under the MIT License.
+About
+replit.com/@subhikh/Adoptera-ett-Husdjur
+Resources
+ Readme
+License
+ MIT license
+ Activity
+Stars
+ 0 stars
+Watchers
+ 1 watching
+Forks
+ 0 forks
+Releases
+No releases published
+Create a new release
+Packages
+No packages published
+Publish your first package
+Languages
+Python100.0%
+Suggested workflows
+Based on your tech stack
+Pylint
+Configure Pylint
+Lint a Python application with pylint.
+Publish Python Package
+Configure Publish Python Package
+Publish a Python Package to PyPI on release.
+Python application
+Configure Python application
+Create and test a Python application.
+More workflows
+Footer
+© 2024 GitHub, Inc.
--- app.py
+++ app.py
@@ -1,24 +1,89 @@
+"""
+  app.py - This file contains the main Flask application for adopting pets.
+  
+  The app allows users to browse and adopt pets such as dogs, cats, and rabbits through different routes.
+  Routes include:
+  - '/' : Displays the home page with links to different pet types.
+  - '/animals/<string:pet_type>' : Displays a list of pets based on the given pet type.
+  - '/animals/<string:pet_type>/<int:pet_id>' : Displays details about a specific pet based on its type and ID.
+  
+  The Flask server is run with debug mode enabled.
+"""
+
 from flask import Flask
+
+from helper import pets
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+  """
+  This function displays the home page for adopting a pet.
+  
+  Returns:
+      str: The HTML content for the home page with links to different pet types.
+  """
   return '''
-  <h1>Adopt a pet!</h1>
-  <p>Browse through the links below to find your new furry friend:</p>
-  <ul>
-      <li><a href="/animals/dogs">Dogs</a></li>
-      <li><a href="/animals/cats">Cats</a></li>
-      <li><a href="/animals/kaniner">Kaniner</a></li>
-  </ul>
-'''
+  <h1>Adopt a Pet<h1/>
+  <p>Browse through the links below to find your new furry friend:<p/> 
+<ul>
+<li><a href='/animals/dogs'>Dogs</a></li> 
+<li><a href='/animals/cats'>Cats </a> </li> 
+<li><a href='/animals/rabbits'>Rabbits</a></li> 
+</ul>
+
+  '''
 
 
-@app.route('/animals/pet_type')
+# Route to display the list of pets based on pet type
+@app.route('/animals/<string:pet_type>')
 def animals(pet_type):
-# Viktigt: Denna kodrad ska alltid placeras längst ner i filen.
-# Detta för att säkerställa en korrekt uppstart av servern.
+  """
+  This function displays a list of pets based on the given pet type.
+  
+  Args:
+      pet_type (str): The type of pet for which the list is displayed.
+      
+  Returns:
+      str: The HTML content for displaying the list of pets of the specified type.
+  """
+  html = f'<h1>List of {pet_type}<h1/>'
+  html += '<ul>'
+  for id, pet in enumerate(pets[pet_type]):
+    html += f'<li><a href="/animals/{pet_type}/{id}">{pet["name"]}</a></li>'
+  return html
 
+
+# Route to display details about a specific pet
+@app.route('/animals/<string:pet_type>/<int:pet_id>')
+def pet(pet_id, pet_type):
+  """
+  This function displays details about a specific pet based on its type and ID.
+  
+  Args:
+      pet_id (int): The ID of the specific pet.
+      pet_type (str): The type of the pet.
+      
+  Returns:
+      str: The HTML content for displaying the details of the specific pet.
+  """
+  list_pet_type = pets[pet_type]
+  pet = list_pet_type[pet_id]
+  return f'''
+  <h1> {pet["name"]}</h1>
+
+  <img src={pet["url"]} /> 
+
+  <p>{pet["description"]}</p>
+
+  <ul>
+    <li>{pet["breed"]} </li>
+    <li>{pet["age"]} </li>
+  </ul>
+  '''
+
+
+# This line is important for starting the Flask server
 app.run(debug=True, host="0.0.0.0")
